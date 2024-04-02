890b45c622089bb48fe4ef81be1a03659d9dd158
sardor ismatullaev <sardorismatullaev905@gmail.com>
2024-04-03 01:37:31 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import "./App.css";
+import questions from "./constants/questions.json";
+import Question from "./components/question";
+import Result from "./components/result";
+
+function App() {
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [userAnswers, setUserAnswers] = useState([]);
+
+  const handleNextQuestion = (isCorrect) => {
+    setCurrentQuestion(currentQuestion + 1);
+    setUserAnswers([...userAnswers, isCorrect]);
+  };
+
+  const resetQuiz = () => {
+    setCurrentQuestion(0);
+    setUserAnswers([]);
+  };
+
+  return (
+    <div className="App">
+      <h1 className="title">World Quiz</h1>
+
+      {/* Questions Component */}
+      {currentQuestion < questions.length && (
+        <Question
+          question={questions[currentQuestion]}
+          onAnswerClick={handleNextQuestion}
+        />
+      )}
+
+      {/* Result Component */}
+      {currentQuestion === questions.length && (
+        <Result
+          userAnswers={userAnswers}
+          questions={questions}
+          resetQuiz={resetQuiz}
+        />
+      )}
+    </div>
+  );
+}
+
+export default App;
